600c9980328525fe252b07c0ab34c7754d4009f2
alokkksharmaa <aloksharma1097@gmail.com>
2026-06-21 00:31:58 +0530

    gitignore updated

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -26,24 +26,24 @@ const initFirebase = () => {
 
   try {
     const serviceAccount = {
-      type:           'service_account',
-      project_id:     process.env.FIREBASE_PROJECT_ID,
+      type: 'service_account',
+      project_id: process.env.FIREBASE_PROJECT_ID,
       private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key:    privateKey,
-      client_email:   process.env.FIREBASE_CLIENT_EMAIL,
-      client_id:      process.env.FIREBASE_CLIENT_ID,
-      auth_uri:       process.env.FIREBASE_AUTH_URI,
-      token_uri:      process.env.FIREBASE_TOKEN_URI,
+      private_key: privateKey,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_AUTH_URI,
+      token_uri: process.env.FIREBASE_TOKEN_URI,
     };
 
     admin.initializeApp({
-      credential:    admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(serviceAccount),
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     });
 
-    _db      = admin.firestore();
+    _db = admin.firestore();
     _storage = admin.storage();
-    _auth    = admin.auth();
+    _auth = admin.auth();
     _db.settings({ ignoreUndefinedProperties: true });
     logger.info('Firebase Admin SDK initialised');
   } catch (err) {
